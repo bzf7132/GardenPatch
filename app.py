713183bb58ca839f9f00f480f0b42a7e0cfdbb42
Scayalet <270301797@yoobeestudent.ac.nz>
2023-12-04 21:33:25 +1300

    Added pages
    Also played around with text size and colour for nav bar.

--- app.py
+++ app.py
@@ -14,3 +14,19 @@ def account():
 @app.route("/loginregister/")
 def loginregister():
     return render_template("loginregister.html")
+
+@app.route("/seedlibrary/")
+def seedLibrary():
+    return render_template("seedLibrary.html")
+
+@app.route("/myPlants/")
+def myPlants():
+    return render_template("myPlants.html")
+
+@app.route("/tasks/")
+def tasks():
+    return render_template("tasks.html")
+
+@app.route("/calendar/")
+def calendar():
+    return render_template("calendar.html")
